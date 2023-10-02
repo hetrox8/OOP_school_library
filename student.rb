810eb1b31546708c43ec381f5851f62505ebca1a
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-10-02 21:49:27 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(name: 'Unknown', age: nil, parent_permission: true, classroom: 'Unknown')
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    '¯\\(ツ)/¯'
   end
 end
